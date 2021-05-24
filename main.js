4d0a5d4ec5b84274fadde5c6d844c8de09e1c798
Saurabh Kumar <43825814+saurabhkumar23@users.noreply.github.com>
2021-05-24 13:10:45 +0530

    scroll down

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ async function fn(){
         // go to youtube playlist
         await page.goto("https://www.youtube.com/playlist?list=PLzkuLC6Yvumv_Rd5apfPRWEcjf9b1JRnq",{waitUntil:"networkidle2"});
         
-        // fetch array containing noOfVideos and noOfViews and extract.
+        // fetch array containing noOfVideos and noOfViews 
         await page.waitForSelector("#stats>.style-scope.ytd-playlist-sidebar-primary-info-renderer", { visible: true });
         await page.waitForSelector("h1#title",{ visible: true });
         let obj = await page.evaluate(function() {
@@ -36,10 +36,26 @@ async function fn(){
         // get noOfVideos in numerical form
         let noOfVideos = obj.nfVideos.split(" ")[0];
         noOfVideos = Number(noOfVideos);
+        
+        // first of all we have to scroll down until data finished so that everything rendered successfully
+        // then we will extract data.
+        // noOFVideos = 783, page render 100 videos at a time. so we need atleast 7 scrolls (783/100)
+        // cVideos = 0 : no of videos rendered till scroll
+        let i = 0;
+        while ((noOfVideos - cVideos) > 100) {
+            await scrollDown(page);
+            console.log(i);
+            i++;
+        }
+        // await page.waitForNavigation({ waitUntil: "networkidle0" });
+        // 83 
+        await waitTillHTMLRendered(page);
+        await scrollDown();
+        console.log(cVideos);
 
-        console.log(obj)
 
-        // fetch video-title array and duration array
+        // now we can select all videos
+        // fetch video-title and duration from all the videos
         let videoSelector = "#video-title";
         let duration = "span.style-scope.ytd-thumbnail-overlay-time-status-renderer";
         await page.waitForSelector("#video-title", { visible: true });
@@ -50,6 +66,16 @@ async function fn(){
     catch (err) {
         console.log(err);
     }
+}
+
+// scroll
+async function scrollDown() {
+    let length = await page.evaluate(function(){
+        let titleElems = document.querySelectorAll("#video-title");   // fetch all rendered videos till that moment.
+        titleElems[titleElems.length-1].scrollIntoView(true);       // target the last video and scroll till that video.
+        return titleElems.length;
+    });
+    cVideos = length;          // update the cVideos
 }
 
 // duration, title array 
